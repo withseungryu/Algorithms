e7376c72974359388fd37c8efb861f6030694370
withseungryu <31756398+withseungryu@users.noreply.github.com>
2020-08-16 20:48:10 +0900

    Create 5.cpp

---
+++ BackJoon/NandM/5.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> arr;
+vector<int> base;
+vector<bool> visited;
+
+void backtrack(int len) {
+
+	if (len == M) {
+		for (int i = 0; i < M; ++i) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+
+	}
+	else {
+		for (int i = 1; i <= N; ++i) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[len] = base[i];
+				backtrack(len + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+
+
+}
+
+int main() {
+
+
+	scanf("%d %d", &N, &M);
+	base.push_back(0);
+	for (int i = 0; i < N; ++i) {
+		int tmp;
+		cin >> tmp;
+		base.push_back(tmp);
+	}
+
+	visited = vector<bool>(N + 1, false);
+	sort(base.begin(), base.end());
+	arr = vector<int>(N + 1, 0);
+	backtrack(0);
+
+
+
+
+
+}
